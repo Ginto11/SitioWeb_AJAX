4ba9aef028cd8f2b4a6ae95875314532a54695eb
ginto11 <salinitosnelson@gmail.com>
2022-07-16 11:52:29 -0500

    Subida de Proyecto HTML

--- js/script.js
+++ js/script.js
@@ -26,35 +26,35 @@ function getPage(url, object){
 
 document.addEventListener("DOMContentLoaded", () =>{
     document.querySelector(".articulo").style.transform = "scale(0.7)"
-    getPage("http://localhost/JS_Curso/AJAX/Proyectos/Pagina_Interactiva/inicio.php")
+    getPage("http://localhost/JS_Curso/AJAX/Proyectos/Pagina_Interactiva/inicio.html")
     agrandar()
 })
 
 $acerca.addEventListener("click", (e) =>{
     e.preventDefault()
     document.querySelector(".articulo").style.transform = "scale(0.7)"
-    getPage("http://localhost/JS_Curso/AJAX/Proyectos/Pagina_Interactiva/acerca.php")
+    getPage("http://localhost/JS_Curso/AJAX/Proyectos/Pagina_Interactiva/acerca.html")
     agrandar()
 })
 
 $inicio.addEventListener("click", (e) =>{
     e.preventDefault()
     document.querySelector(".articulo").style.transform = "scale(0.7)"
-    getPage("http://localhost/JS_Curso/AJAX/Proyectos/Pagina_Interactiva/inicio.php")
+    getPage("http://localhost/JS_Curso/AJAX/Proyectos/Pagina_Interactiva/inicio.html")
     agrandar()
 })
 
 $informacion.addEventListener("click", (e) =>{
     e.preventDefault()
     document.querySelector(".articulo").style.transform = "scale(0.7)"
-    getPage("http://localhost/JS_Curso/AJAX/Proyectos/Pagina_Interactiva/informacion.php")
+    getPage("http://localhost/JS_Curso/AJAX/Proyectos/Pagina_Interactiva/informacion.html")
     agrandar()
 })
 
 $contacto.addEventListener("click", (e) =>{
     e.preventDefault()
     document.querySelector(".articulo").style.transform = "scale(0.7)"
-    getPage("http://localhost/JS_Curso/AJAX/Proyectos/Pagina_Interactiva/contacto.php"  )
+    getPage("http://localhost/JS_Curso/AJAX/Proyectos/Pagina_Interactiva/contacto.html")
     agrandar()
 })
 
